a491dc7ea926ee8dad8e091b684cec11a18eda2c
miguzo <54639356+miguzo@users.noreply.github.com>
2025-11-24 15:40:14 +0100

    Update page.tsx

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -63,7 +63,7 @@ export default function VideoPage() {
             src="https://www.youtube.com/embed/rTYdjkZaPh0?controls=0&modestbranding=1&rel=0&showinfo=0"
             style={{
               width: "80%",
-              height: "55%",
+              height: "54%",
               border: "none",
               transform: "translateX(3%) rotateY(20deg)",
             }}
