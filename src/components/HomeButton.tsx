51251eb7a291e88221ae1d866aac800fd76eafe1
miguzo <54639356+miguzo@users.noreply.github.com>
2026-02-09 10:22:23 +0100

    Update HomeButton.tsx

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 export default function HomeButton() {
   const router = useRouter();
 
+ 
   return (
     <button
       onClick={() => router.push("/")}
@@ -12,7 +13,8 @@ export default function HomeButton() {
         left: "4%",     // move left-right
         top: "4%",      // move up-down
         width: "12%",   // size
-        height: "7%",   // size
+        height: "7%", 
+          // size
         background: "transparent",
         border: "none",
         cursor: "pointer",
